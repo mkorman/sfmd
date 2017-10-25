5d3a7e5e717bcfdacb5218af556829fbac2dfb86
mkorman <mkormanc@gmail.com>
2017-10-25 10:38:06 +0100

    Created metadata client class
    - Facade for the 2 WSDLs
    - Added code to read credentials from settings
    - Simple test to read data

---
+++ sfmd/MetadataClient.cs
@@ -0,0 +1,48 @@
+﻿using sfmd.SalesforceMetadataClient;
+using System;
+using System.Linq;
+using System.Net;
+using sfmd.SalesforceEnterpriseClient;
+using SessionHeader = sfmd.SalesforceMetadataClient.SessionHeader;
+
+namespace sfmd
+{
+    class MetadataClient
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public string OrgId { get; set; }
+        public string SessionId { get; set; }
+        public string MetadataUrl { get; set; }
+
+        private MetadataPortTypeClient client;
+
+        public MetadataClient()
+        {
+            // SF requires TLS 1.1 or 1.2
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+        }
+
+        public void Login()
+        {
+            var soapClient = new SoapClient();
+            var loginResult = soapClient.login(null, Username, Password + Token);
+
+            SessionId = loginResult.sessionId;
+            MetadataUrl = loginResult.metadataServerUrl;
+            //Console.WriteLine($"Logged in. Session ID is {SessionId} and URL is {MetadataUrl}");
+        }
+
+        public Metadata[] GetMetadata(string type, string[] names)
+        {
+            client = new MetadataPortTypeClient("Metadata", MetadataUrl);
+            var sessionHeader = new SessionHeader { sessionId = SessionId };
+            var metadata = client.readMetadata(sessionHeader, null, type, names);
+            metadata.ToList().ForEach(md => Console.WriteLine(md.fullName));
+            return metadata;
+            //metadata = client.readMetadata(sessionHeader, null, "ApexClass", new[] { "ActionProcessor", "AgentDetails" });
+            //metadata.ToList().ForEach(md => Console.WriteLine($"Class: {md.fullName}, ApiVersion: {((ApexClass)md).apiVersion}"));
+        }
+    }
+}
--- sfmd/Program.cs
+++ sfmd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,24 @@ namespace sfmd
 {
     class Program
     {
+
+        private static MetadataClient CreateMetadataClient()
+        {
+            return new MetadataClient
+            {
+                Username = ConfigurationManager.AppSettings["username"],
+                Password = ConfigurationManager.AppSettings["password"],
+                Token = ConfigurationManager.AppSettings["token"]
+            };
+        }
+
         static void Main(string[] args)
         {
+            var metadataClient = CreateMetadataClient();
+            metadataClient.Login();
+            metadataClient.GetMetadata("CustomObject", new[] { "NvmNodes__c", "Contact", "Account" });
+            //metadataClient.GetMetadata("CustomObject", new[] { "Contact" });
+            Console.ReadLine();
         }
     }
 }
